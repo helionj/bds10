3433a4ba629d3f0efc13201606a7d854e1780258
helionporto <helionporto@hotmail.com>
2021-08-30 22:58:53 -0400

    Tests passed

--- frontweb/src/pages/Admin/Employees/Form/index.tsx
+++ frontweb/src/pages/Admin/Employees/Form/index.tsx
@@ -1,11 +1,55 @@
+import { AxiosRequestConfig } from 'axios';
+import { useEffect, useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
 import { useHistory } from 'react-router-dom';
+import Select from 'react-select';
+import { toast } from 'react-toastify';
+import { Department } from 'types/department';
+import { Employee } from 'types/employee';
+import { requestBackend } from 'util/requests';
 import './styles.css';
 
 const Form = () => {
-  
+
   const history = useHistory();
+
+
+
+  const [selectDepartments, setSelectDepartments] = useState<Department[]>([]);
+   
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<Employee>();
+
   const handleCancel = () => {
     history.push('/admin/employees');
+  };
+
+  useEffect(() => {
+    requestBackend({method: 'GET', url: '/departments',withCredentials: true}).then((response) => {
+      setSelectDepartments(response.data);
+    });
+  }, []);
+
+
+  const onSubmit = (formData: Employee) => {
+  
+    const config: AxiosRequestConfig = {
+      method: 'POST',
+      url: '/employees',
+      data: formData,
+      withCredentials: true,
+    };
+    requestBackend(config).then(() => {
+      toast.info('Cadastrado com sucesso');
+      history.push('/admin/employees');
+    })
+    .catch(()=> {
+      toast.error("Erro ao cadastrar");
+    });
   };
 
   return (
@@ -13,27 +57,76 @@ const Form = () => {
       <div className="base-card employee-crud-form-card">
         <h1 className="employee-crud-form-title">INFORME OS DADOS</h1>
 
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)} data-testid="form">
           <div className="row employee-crud-inputs-container">
             <div className="col employee-crud-inputs-left-container">
-
               <div className="margin-bottom-30">
-                <input type="text" 
-                  className="form-control base-input is-invalid"
+                <input
+                  {...register('name', {
+                    required: 'Campo obrigatório',
+                  })}
+                  type="text"
+                  className={`form-control base-input ${
+                    errors.name ? 'is-invalid' : ''
+                  }`}
+                  placeholder="Nome do funcionário"
+                  name="name"
+                  data-testid="name"
                 />
                 <div className="invalid-feedback d-block">
-                  Mensagem de erro
+                  {errors.name?.message}
                 </div>
               </div>
 
               <div className="margin-bottom-30">
-                <input type="text" 
-                  className="form-control base-input"
+                <input
+                  {...register('email', {
+                    required: 'Campo obrigatório',
+                    pattern: {
+                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                      message: 'Email inválido',
+                    },
+                  })}
+                  type="text"
+                  className={`form-control base-input ${
+                    errors.name ? 'is-invalid' : ''
+                  }`}
+                  placeholder="Email do funcionário"
+                  name="email"
+                  data-testid="email"
                 />
                 <div className="invalid-feedback d-block">
-                  
+                  {errors.email?.message}
                 </div>
               </div>
+
+              <div className="margin-bottom-30">
+                <label htmlFor="department" className="d-none">Departamento</label>
+                <Controller
+                  name="department"
+                  rules={{ required: true }}
+                  control={control}
+                  render={({ field }) => (
+                    <Select
+                      {...field}
+                      options={selectDepartments}
+                      getOptionLabel={(department: Department) => department.name}
+                      getOptionValue={(department: Department) =>
+                        String(department.id)
+                      }
+                      classNamePrefix="employee-crud-select"
+                      placeholder="Departamento"
+                      inputId="department"
+                    />
+                  )}
+                />
+                {errors.department && (
+                  <div className="invalid-feedback d-block">
+                    Campo obrigatório
+                  </div>
+                )}
+              </div>
+
 
             </div>
           </div>
--- frontweb/src/pages/Admin/Employees/List/index.tsx
+++ frontweb/src/pages/Admin/Employees/List/index.tsx
@@ -3,40 +3,71 @@ import './styles.css';
 import Pagination from 'components/Pagination';
 import EmployeeCard from 'components/EmployeeCard';
 import { Link } from 'react-router-dom';
+import { useCallback, useEffect, useState } from 'react';
+import { requestBackend } from 'util/requests';
+import { AxiosRequestConfig } from 'axios';
+import { SpringPage } from 'types/vendor/spring';
+import { Employee } from 'types/employee';
+import { hasAnyRoles } from 'util/auth';
 
-const employeeHardCode = { // delete
-  id: 1,
-  name: "Carlos",
-  email: "carlos@gmail.com",
-  department: {
-    id: 1,
-    name: "Sales"
-  }
+type ControlComponentsData = {
+  activePage: number;
 };
 
 const List = () => {
+  const [controlComponentsData, setControlComponentsData] =
+    useState<ControlComponentsData>({
+      activePage: 0,
+    });
 
   const handlePageChange = (pageNumber: number) => {
-    // to do
+    setControlComponentsData({ activePage: pageNumber });
   };
+
+  const [page, setPage] = useState<SpringPage<Employee>>();
+
+  const getEmployees = useCallback(() => {
+    const config: AxiosRequestConfig = {
+      method: 'GET',
+      url: '/employees',
+      params: {
+        page: controlComponentsData.activePage,
+        size: 6,
+      },
+      withCredentials: true,
+    };
+
+    requestBackend(config).then((response) => {
+      setPage(response.data);
+    });
+  }, [controlComponentsData]);
+
+  useEffect(() => {
+    getEmployees();
+  }, [getEmployees]);
 
   return (
     <>
-      <Link to="/admin/employees/create">
-        <button className="btn btn-primary text-white btn-crud-add">
-          ADICIONAR
-        </button>
-      </Link>
+      {hasAnyRoles(['ROLE_ADMIN']) && (
+        <Link to="/admin/employees/create">
+          <button className="btn btn-primary text-white btn-crud-add">
+            ADICIONAR
+          </button>
+        </Link>
+      )}
 
-      <EmployeeCard employee={employeeHardCode} />
-      <EmployeeCard employee={employeeHardCode} />
-      <EmployeeCard employee={employeeHardCode} />
-      <EmployeeCard employee={employeeHardCode} />
+      <div className="row">
+        {page?.content.map((employee) => (
+          <div key={employee.id}>
+            <EmployeeCard employee={employee} />
+          </div>
+        ))}
+      </div>
 
       <Pagination
-        forcePage={0}
-        pageCount={1}
+        pageCount={page ? page?.totalPages : 0}
         range={3}
+        forcePage={page?.number}
         onChange={handlePageChange}
       />
     </>
